30cab2a1f46dab1391f4a42ae18da36ae06e5f58
周康 <dev.zhoukang@gmail.com>
2018-11-30 18:27:42 +0800

    Update: Setup page ui

--- zk_flutter_demo_02/lib/views/second_page.dart
+++ zk_flutter_demo_02/lib/views/second_page.dart
@@ -12,9 +12,46 @@ class SecondPageState extends State<SecondPage> {
       appBar: AppBar(
         title: Text('Second page'),
       ),
-      body: Center(
-        child: Text('This is the second page'),
+      body: Container(
+        padding: EdgeInsets.all(0),
+        child: ZKListView()
       ),
     );
   }
+}
+
+class ZKListView extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      return ZKListViewState();
+    }
+}
+
+class ZKListViewState extends State<ZKListView> {
+  @override
+    Widget build(BuildContext context) {
+      return ListView.builder(
+        itemCount: 100,
+        itemBuilder: (BuildContext context, int i) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text('我是周杰伦', style: TextStyle(color: Color(0x88222222), fontSize: 13)),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('我在北京等你', style: TextStyle(color: Color(0xff222222), fontSize: 15)),
+                  ),
+                ],
+              ),
+            ],
+          );
+        }
+      );
+    }
 }
