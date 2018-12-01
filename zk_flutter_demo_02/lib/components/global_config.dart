71d14b1e636356b8222783e73fb8d79493a83f18
周康 <dev.zhoukang@gmail.com>
2018-12-01 15:28:26 +0800

    Update: Page UI

---
+++ zk_flutter_demo_02/lib/components/global_config.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class GlobalConfig {
+  static var global_color_theme = Color(0xff096096);
+}
--- zk_flutter_demo_02/lib/views/second_page.dart
+++ zk_flutter_demo_02/lib/views/second_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/global_config.dart';
 
 class SecondPage extends StatefulWidget {
   @override
@@ -30,28 +31,38 @@ class ZKListView extends StatefulWidget {
 class ZKListViewState extends State<ZKListView> {
   @override
     Widget build(BuildContext context) {
-      return ListView.builder(
-        itemCount: 100,
-        itemBuilder: (BuildContext context, int i) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
+      return Container(
+        color: GlobalConfig.global_color_theme,
+        child: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (BuildContext context, int i) {
+            return Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(6)), color: Colors.white),
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 6.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('我是周杰伦', style: TextStyle(color: Color(0x88222222), fontSize: 13)),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('我在北京等你', style: TextStyle(color: Color(0xff222222), fontSize: 15)),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('我是周杰伦', style: TextStyle(color: Color(0x88222222), fontSize: 13)),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('我在北京等你', style: TextStyle(color: Color(0xff222222), fontSize: 15)),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          );
-        }
+            );
+          }
+        ),
       );
     }
 }
