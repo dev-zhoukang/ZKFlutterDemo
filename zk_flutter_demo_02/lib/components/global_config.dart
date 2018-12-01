7b33c475aa835182e2dde9cf19c86d435b54a8b4
周康 <dev.zhoukang@gmail.com>
2018-12-01 16:53:53 +0800

    Update: Sync

--- zk_flutter_demo_02/lib/components/global_config.dart
+++ zk_flutter_demo_02/lib/components/global_config.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 class GlobalConfig {
-  static var global_color_theme = Color(0xff096096);
+  static const Color global_color_theme = const Color(0xff096096);
+  static const Color global_color_title = const Color(0xff222222);
+  static const Color global_color_subtitle = const Color(0x88222222);
 }
--- zk_flutter_demo_02/lib/views/second_page.dart
+++ zk_flutter_demo_02/lib/views/second_page.dart
@@ -12,57 +12,74 @@ class SecondPageState extends State<SecondPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Second page'),
+        backgroundColor: GlobalConfig.global_color_theme,
       ),
       body: Container(
-        padding: EdgeInsets.all(0),
-        child: ZKListView()
-      ),
+          decoration: BoxDecoration(color: GlobalConfig.global_color_theme),
+          child: ZKListView()),
     );
   }
 }
 
 class ZKListView extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return ZKListViewState();
-    }
+  State<StatefulWidget> createState() {
+    return ZKListViewState();
+  }
 }
 
 class ZKListViewState extends State<ZKListView> {
   @override
-    Widget build(BuildContext context) {
-      return Container(
-        color: GlobalConfig.global_color_theme,
-        child: ListView.builder(
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
           itemCount: 100,
           itemBuilder: (BuildContext context, int i) {
             return Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(6)), color: Colors.white),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                  color: Colors.white),
               margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 6.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: Row(
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text('我是周杰伦', style: TextStyle(color: Color(0x88222222), fontSize: 13)),
-                      ),
-                    ],
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                      height: 80,
+                    ),
                   ),
-                  Row(
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text('我在北京等你', style: TextStyle(color: Color(0xff222222), fontSize: 15)),
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('我是周杰伦',
+                                style: TextStyle(
+                                    color: GlobalConfig.global_color_subtitle,
+                                    fontSize: 13)),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('我在北京等你',
+                                style: TextStyle(
+                                    color: GlobalConfig.global_color_title,
+                                    fontSize: 15)),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
               ),
             );
-          }
-        ),
-      );
-    }
+          }),
+    );
+  }
 }
