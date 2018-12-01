d07e951f707063ab5e357ab1a9be526961f7e96b
周康 <dev.zhoukang@gmail.com>
2018-12-01 17:56:07 +0800

    Add: Img

--- zk_flutter_demo_03/lib/main.dart
+++ zk_flutter_demo_03/lib/main.dart
@@ -3,58 +3,93 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: '周杰伦',
-        home: HomePage(),
-      );
-    }
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '周杰伦',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: HomePage(),
+    );
+  }
 }
 
 class HomePage extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      Widget titleSection = Container(
-        padding: EdgeInsets.all(32.0),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                      '周杰伦',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    '雨纷纷，旧故里草木深',
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: EdgeInsets.all(32.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    '周杰伦',
                     style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
+                ),
+                Text(
+                  '雨纷纷，旧故里草木深',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 14,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('88'),
+        ],
+      ),
+    );
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
               ),
             ),
-            Icon(
-              Icons.star,
-              color: Colors.red[500],
-            ),
-            Text('88'),
-          ],
-        ),
-      );
-
-      return Material(
-        child: titleSection,
+          ),
+        ],
       );
     }
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, 'Call'),
+          buildButtonColumn(Icons.near_me, 'Route'),
+          buildButtonColumn(Icons.share, 'Share'),
+        ],
+      ),
+    );
+
+    return Material(
+      child: titleSection,
+    );
+  }
 }
