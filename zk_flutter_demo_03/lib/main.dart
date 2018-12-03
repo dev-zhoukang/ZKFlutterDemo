12be75cc9826b283f13a22b6c7395e1f98b27906
周康 <dev.zhoukang@gmail.com>
2018-12-03 17:53:44 +0800

    Update: Add Action

--- zk_flutter_demo_03/lib/main.dart
+++ zk_flutter_demo_03/lib/main.dart
@@ -16,11 +16,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() {
+    return _FavoriteWidgetState();
+  }
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          child: IconButton(
+            icon: _isFavorited ? Icon(Icons.star) : Icon(Icons.star_border),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(),
+        
+      ],
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
-      padding: const EdgeInsets.only(left: 32.0,top: 32.0, right: 32.0, bottom: 8),
+      padding:
+          const EdgeInsets.only(left: 32.0, top: 32.0, right: 32.0, bottom: 8),
       child: Row(
         children: [
           Expanded(
