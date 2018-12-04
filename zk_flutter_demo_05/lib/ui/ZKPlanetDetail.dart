4d3dec058c169a77bb86a6b19c7542543263cfb0
周康 <dev.zhoukang@gmail.com>
2018-12-04 21:16:10 +0800

    Update: Build detail UI

--- zk_flutter_demo_05/lib/ui/ZKPlanetDetail.dart
+++ zk_flutter_demo_05/lib/ui/ZKPlanetDetail.dart
@@ -5,28 +5,56 @@ class ZKPlanetDetail extends StatelessWidget {
   final ZKPlanetModel model;
   ZKPlanetDetail(this.model);
 
+  final buildBgImageView = Container(
+    alignment: FractionalOffset.topCenter,
+    child: Image(
+      image: NetworkImage(
+          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543934675275&di=af293a065f4dbbe2a22711a8264e2bb7&imgtype=0&src=http%3A%2F%2Fbbsfiles.vivo.com.cn%2Fvivobbs%2Fattachment%2Fforum%2F201702%2F21%2F093712l1i9fo1op8iosdsn.jpg'),
+    ),
+  );
+
+  final buildGradientBgView = Container(
+    alignment: FractionalOffset.bottomCenter,
+    height: 200.0,
+    decoration: BoxDecoration(
+        gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: <Color>[Colors.transparent, Colors.blueAccent],
+    )),
+  );
+
+  final buildCardView = Container(
+    height: 100,
+    width: 200,
+    alignment: Alignment.bottomCenter,
+    decoration: BoxDecoration(color: Color(0xff303066)),
+    child: Column(
+      children: <Widget>[
+        Text('火星', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400)),
+        Text('火影描述', style: TextStyle(color: Colors.white54, fontSize: 15)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(Icons.local_airport, color: Colors.white54),
+            SizedBox(width: 5),
+            Text('3.00', style: TextStyle(color: Colors.white54, fontSize: 13)),
+          ],
+        ),
+      ],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('详情页')),
-        body: Center(
-            child: Column(
-          children: <Widget>[
-            FloatingActionButton(
-              child: Icon(Icons.arrow_back_ios),
-              onPressed: () => Navigator.pop(context),
-            ),
-            Text(model.name,
-                style: TextStyle(
-                    color: Colors.purple,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold)),
-            Hero(
-              tag: 'planet-hero-${model.id}',
-              child: Image(
-                  image: AssetImage(model.image), width: 96, height: 96),
-            ),
-          ],
-        )));
+      body: Stack(
+        children: <Widget>[
+          buildBgImageView,
+          buildCardView,
+        ],
+      ),
+    );
   }
 }
