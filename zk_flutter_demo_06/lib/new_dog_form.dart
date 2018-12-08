4f8671c5b1d34d821dcfd5f71c1131ad78c3ab07
周康 <dev.zhoukang@gmail.com>
2018-12-08 17:24:52 +0800

    Update: Show snackBar

--- zk_flutter_demo_06/lib/new_dog_form.dart
+++ zk_flutter_demo_06/lib/new_dog_form.dart
@@ -16,6 +16,10 @@ class _AddDogFormPageState extends State<AddDogFormPage> {
     print('submitBtnDidClick');
     if (nameController.text.isEmpty) {
       print('name can not be null');
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text('name can not be null'),
+        backgroundColor: Colors.redAccent,
+      ));
     } else {
       Dog newDog = Dog(
           nameController.text, locationController.text, descController.text);
@@ -26,43 +30,46 @@ class _AddDogFormPageState extends State<AddDogFormPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add a dog')),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: nameController,
-              onChanged: (text) => nameController.text = text,
-              decoration: InputDecoration(
-                labelText: '请输入名称',
+        appBar: AppBar(title: Text('Add a dog')),
+        body: Builder(
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: nameController,
+                    onChanged: (text) => nameController.text = text,
+                    decoration: InputDecoration(
+                      labelText: '请输入名称',
+                    ),
+                  ),
+                  SizedBox(height: kDefaultMargin),
+                  TextField(
+                    controller: locationController,
+                    onChanged: (text) => locationController.text = text,
+                    decoration: InputDecoration(
+                      labelText: '请输入地址',
+                    ),
+                  ),
+                  SizedBox(height: kDefaultMargin),
+                  TextField(
+                    controller: descController,
+                    onChanged: (text) => descController.text = text,
+                    decoration: InputDecoration(
+                      labelText: '请输入描述',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                    onPressed: () => submitBtnDidClick(context),
+                    color: Colors.orange,
+                    child: Text('提交', style: TextStyle(color: Colors.white)),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: kDefaultMargin),
-            TextField(
-              controller: locationController,
-              onChanged: (text) => locationController.text = text,
-              decoration: InputDecoration(
-                labelText: '请输入地址',
-              ),
-            ),
-            SizedBox(height: kDefaultMargin),
-            TextField(
-              controller: descController,
-              onChanged: (text) => descController.text = text,
-              decoration: InputDecoration(
-                labelText: '请输入描述',
-              ),
-            ),
-            SizedBox(height: 20),
-            RaisedButton(
-              onPressed: () => submitBtnDidClick(context),
-              color: Colors.orange,
-              child: Text('提交', style: TextStyle(color: Colors.white)),
-            ),
-          ],
-        ),
-      ),
-    );
+            );
+          },
+        ));
   }
 }
